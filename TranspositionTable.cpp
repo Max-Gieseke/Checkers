53f19d4e40377ec3a24729ceaa276b957a84a308
Max <giese405@umn.edu>
2023-11-05 22:22:30 -0600

    Changed cmake

--- TranspositionTable.cpp
+++ TranspositionTable.cpp
@@ -1,38 +1,38 @@
+////
+//// Created by maxgi on 8/22/2023.
+////
 //
-// Created by maxgi on 8/22/2023.
+//#include "TranspositionTable.h"
 //
-
-#include "TranspositionTable.h"
-
-TranspositionTable::TranspositionTable() {
-    hash = ZobristHash();
-}
-
-void TranspositionTable::addValue(std::shared_ptr<MoveNode> node) {
-    unsigned long long int key = hash.calcHash(node->getBoard());
-    table[key] = node;
-}
-
-std::shared_ptr<MoveNode> TranspositionTable::getValue(unsigned long long int key) {
-    return table[key];
-}
-
-unsigned long long int TranspositionTable::computeHash(CheckerBoard b) {
-    return hash.calcHash(b);
-}
-
-bool TranspositionTable::isIn(CheckerBoard b, int depth) {
-    unsigned long long int key = hash.calcHash(b);
-    if(table.count(key) == 0){
-        return false;
-    }
-    if(table[key]->getDepth() < depth){
-        return false;
-    }
-    return true;
-}
-
-std::shared_ptr<MoveNode> TranspositionTable::getNode(CheckerBoard b) {
-    return table[hash.calcHash(b)];
-}
-
+//TranspositionTable::TranspositionTable() {
+//    hash = ZobristHash();
+//}
+//
+//void TranspositionTable::addValue(std::shared_ptr<MoveNode> node) {
+//    unsigned long long int key = hash.calcHash(node->getBoard());
+//    table[key] = node;
+//}
+//
+//std::shared_ptr<MoveNode> TranspositionTable::getValue(unsigned long long int key) {
+//    return table[key];
+//}
+//
+//unsigned long long int TranspositionTable::computeHash(CheckerBoard b) {
+//    return hash.calcHash(b);
+//}
+//
+//bool TranspositionTable::isIn(CheckerBoard b, int depth) {
+//    unsigned long long int key = hash.calcHash(b);
+//    if(table.count(key) == 0){
+//        return false;
+//    }
+//    if(table[key]->getDepth() < depth){
+//        return false;
+//    }
+//    return true;
+//}
+//
+//std::shared_ptr<MoveNode> TranspositionTable::getNode(CheckerBoard b) {
+//    return table[hash.calcHash(b)];
+//}
+//
--- TranspositionTable.h
+++ TranspositionTable.h
@@ -1,27 +1,26 @@
+////
+//// Created by Max Gieseke on 8/22/2023.
+////
 //
-// Created by Max Gieseke on 8/22/2023.
+//#ifndef CHECKERS_TRANSPOSITIONTABLE_H
+//#define CHECKERS_TRANSPOSITIONTABLE_H
+//#include "ZobristHash.h"
+//#include <unordered_map>
 //
-
-#ifndef CHECKERS_TRANSPOSITIONTABLE_H
-#define CHECKERS_TRANSPOSITIONTABLE_H
-#include "ZobristHash.h"
-#include <unordered_map>
-#include "MoveNode.h"
-
-
-class TranspositionTable {
-private:
-    ZobristHash hash;
-    std::unordered_map<unsigned long long int, std::shared_ptr<MoveNode>> table;
-public:
-    TranspositionTable();
-    std::shared_ptr<MoveNode> getValue(unsigned long long int);
-    void addValue(std::shared_ptr<MoveNode>);
-    unsigned long long int computeHash(CheckerBoard);
-    bool isIn(CheckerBoard, int);
-    std::shared_ptr<MoveNode> getNode(CheckerBoard);
-
-};
-
-
-#endif //CHECKERS_TRANSPOSITIONTABLE_H
+//
+//class TranspositionTable {
+//private:
+//    ZobristHash hash;
+//    std::unordered_map<unsigned long long int, std::shared_ptr<MoveNode>> table;
+//public:
+//    TranspositionTable();
+//    std::shared_ptr<MoveNode> getValue(unsigned long long int);
+//    void addValue(std::shared_ptr<MoveNode>);
+//    unsigned long long int computeHash(CheckerBoard);
+//    bool isIn(CheckerBoard, int);
+//    std::shared_ptr<MoveNode> getNode(CheckerBoard);
+//
+//};
+//
+//
+//#endif //CHECKERS_TRANSPOSITIONTABLE_H
